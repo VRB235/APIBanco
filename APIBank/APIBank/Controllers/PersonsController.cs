a0373c2c1f815a4f16fddf3cef2c392b618dce3d
Leonardo Azocar <leonardoazocar23@gmail.com>
2021-07-29 22:14:47 -0400

    Subiendo API ejemplo

---
+++ APIBank/APIBank/Controllers/PersonsController.cs
@@ -0,0 +1,97 @@
+﻿using APIBank.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBank.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        public List<Person> data; 
+        public PersonsController()
+        {
+            data = Mocks.FillData();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetPersons(string email, string phoneNumber)
+        {
+            if(email != null && phoneNumber == null)
+            {
+                return Ok(data.Select(p => new Person
+                {
+                    DocumentID = p.DocumentID,
+                    Email = p.Email,
+                    ID = p.ID,
+                    Name = p.Name,
+                    LastName = p.LastName,
+                    PhoneNumber = p.PhoneNumber
+                }).Where(x=>x.Email == email).FirstOrDefault());
+            }
+            if (email == null && phoneNumber != null)
+            {
+                return Ok(data.Select(p => new Person
+                {
+                    DocumentID = p.DocumentID,
+                    Email = p.Email,
+                    ID = p.ID,
+                    Name = p.Name,
+                    LastName = p.LastName,
+                    PhoneNumber = p.PhoneNumber
+                }).Where(x => x.PhoneNumber == phoneNumber).FirstOrDefault());
+            }
+            if (email == null && phoneNumber == null)
+            {
+                return Ok(data.Select(p => new Person
+                {
+                    DocumentID = p.DocumentID,
+                    Email = p.Email,
+                    ID = p.ID,
+                    Name = p.Name,
+                    LastName = p.LastName,
+                    PhoneNumber = p.PhoneNumber
+                }).ToList());
+            }
+            if (email != null && phoneNumber != null)
+            {
+                return Ok(data.Select(p => new Person
+                {
+                    DocumentID = p.DocumentID,
+                    Email = p.Email,
+                    ID = p.ID,
+                    Name = p.Name,
+                    LastName = p.LastName,
+                    PhoneNumber = p.PhoneNumber
+                }).Where(x => x.Email == email && x.PhoneNumber == phoneNumber).FirstOrDefault());
+            }
+            return Conflict("Error Desconocido");
+        }
+        [HttpGet("{documentId}")]
+        public async Task<IActionResult> GetPerson(string documentId)
+        {
+            return Ok(data.Select(p => new Person
+            {
+                DocumentID = p.DocumentID,
+                Email = p.Email,
+                ID = p.ID,
+                Name = p.Name,
+                LastName = p.LastName,
+                PhoneNumber = p.PhoneNumber
+            }).Where(x=>x.DocumentID == documentId).FirstOrDefault());
+        }
+        [HttpGet("{documentId}/Accounts")]
+        public async Task<IActionResult> GetAccounts(string documentId)
+        {
+            var person = data.Where(p => p.DocumentID == documentId).FirstOrDefault();
+            if(person == null)
+            {
+                return NoContent();
+            }
+            return Ok(person.Accounts);
+        }
+    }
+}
---
+++ APIBank/APIBank/Mocks.cs
@@ -0,0 +1,116 @@
+﻿using APIBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBank
+{
+    public static class Mocks
+    {
+        public static List<Person> FillData()
+        {
+            return new List<Person>
+            {
+                new Person
+                {
+                    ID = 1,
+                    DocumentID = "V24000001",
+                    Email = "juli@gmail.com",
+                    Name = "Julianis",
+                    LastName = "Caldera",
+                    PhoneNumber = "04142031975",
+                    Accounts = new List<Account>
+                    {
+                        new Account
+                        {
+                            ID = 1,
+                            AmountAvailable = 10000,
+                            NameProduct = "CUENTA CORRIENTE",
+                            NumberAccount = "10141234556515000001",
+                            TypeAccount = "BS",
+                            Movements = new List<Movement>
+                            {
+                                new Movement
+                                {
+                                    Reference = 1,
+                                    Amount = 1000,
+                                    Concept =  "Pago de Elextricidad",
+                                    DocumentIdBeneficiary = "J151315155",
+                                    Type = "DB"
+                                }
+                                ,
+                                new Movement
+                                {
+                                    Reference = 2,
+                                    Amount = 2500,
+                                    Concept =  "Pago de Cable",
+                                    DocumentIdBeneficiary = "J56565565",
+                                    Type = "DB"
+                                }
+                                ,
+                                new Movement
+                                {
+                                    Reference = 3,
+                                    Amount = 5000000,
+                                    Concept =  "Transferencia a terceros Banesco",
+                                    DocumentIdBeneficiary = "V24000002",
+                                    Type = "DB"
+                                }
+                            }
+                        }
+                    }
+                },
+                new Person
+                {
+                    ID = 2,
+                    DocumentID = "V24000002",
+                    Email = "andy@gmail.com",
+                    Name = "Andy",
+                    LastName = "Castro",
+                    PhoneNumber = "04145377035",
+                    Accounts = new List<Account>
+                    {
+                        new Account
+                        {
+                            ID = 2,
+                            AmountAvailable = 1500,
+                            NameProduct = "CUENTA CUSTODIA",
+                            NumberAccount = "10141234556515000001",
+                            TypeAccount = "DLS",
+                            Movements = new List<Movement>
+                            {
+                                new Movement
+                                {
+                                    Reference = 3,
+                                    Amount = 5,
+                                    Concept =  "Transferencia a terceros Banesco",
+                                    DocumentIdBeneficiary = "V24000001",
+                                    Type = "CR"
+                                }
+                                ,
+                                new Movement
+                                {
+                                    Reference = 5,
+                                    Amount = 100,
+                                    Concept =  "Operacion de cambio (VENTA)",
+                                    DocumentIdBeneficiary = "V24000002",
+                                    Type = "DB"
+                                }
+                                ,
+                                new Movement
+                                {
+                                    Reference = 6,
+                                    Amount = 1.5,
+                                    Concept =  "Cobro por Mantenimiento",
+                                    DocumentIdBeneficiary = "0",
+                                    Type = "DB"
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
